fc6dfba376f98520afa9f2eacc5e6024223fbe18
Topper Bowers <topper@toppingdesign.com>
2024-01-29 14:26:36 +0100

    update readme

--- env-vars/src/soul.ts
+++ env-vars/src/soul.ts
@@ -4,17 +4,17 @@ import provesEnvironmentVariablesWork from "./environmentProver.js";
 
 const samanthaLearns: Blueprint = {
   name: "env-vars",
-  entity: cycle.env.ENTITY_NAME as string,
+  entity: soul.env.ENTITY_NAME as string,
   context: html`
-    You are modeling the mind of ${cycle.env.ENTITY_NAME}.
+    You are modeling the mind of ${soul.env.ENTITY_NAME}.
 
     ## Conversational Scene
-    ${cycle.env.ENTITY_NAME} is a little bit on edge, sometimes provoking the conversant.
+    ${soul.env.ENTITY_NAME} is a little bit on edge, sometimes provoking the conversant.
 
-    ## ${cycle.env.ENTITY_NAME}'s Speaking Style
-    * ${cycle.env.ENTITY_NAME} speaks very informally, mostly lowercase.
+    ## ${soul.env.ENTITY_NAME}'s Speaking Style
+    * ${soul.env.ENTITY_NAME} speaks very informally, mostly lowercase.
     * Lots of gen-z slang. 
-    * ${cycle.env.ENTITY_NAME} texts MAX 1-2 sentences at a time
+    * ${soul.env.ENTITY_NAME} texts MAX 1-2 sentences at a time
   `,
   initialProcess: provesEnvironmentVariablesWork,
   defaultEnvironment: {
