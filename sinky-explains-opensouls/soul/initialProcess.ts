4c792bfff6a7134e2fc4aa141a3c28d50e18ea1a
Topper Bowers <topper@toppingdesign.com>
2024-02-02 00:15:02 +0100

    change around sinky

--- sinky-explains-opensouls/soul/initialProcess.ts
+++ sinky-explains-opensouls/soul/initialProcess.ts
@@ -1,19 +1,19 @@
 
 import { externalDialog } from "socialagi";
 import { MentalProcess, useActions, useProcessManager, useSoulMemory } from "soul-engine";
-import rememberThings from "./mentalProcesses/rememberThings.js";
+import pitchesTheSoulEngine from "./mentalProcesses/pitchesTheSoulEngine.js";
 
 const sagtHallo: MentalProcess = async ({ step: initialStep }) => {
   const { speak  } = useActions()
   const { setNextProcess } = useProcessManager()
 
   const { stream, nextStep } = await initialStep.next(
-    externalDialog("Tell that user that they are about to learn everything about this app."),
+    externalDialog("Introduce yourself to the user, see if you can find their name."),
     { stream: true }
   );
   speak(stream);
 
-  setNextProcess(rememberThings)
+  setNextProcess(pitchesTheSoulEngine)
 
   return nextStep
 }
