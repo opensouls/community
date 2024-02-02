9f76e13a13c75ac38ebcc05392877a98c3d6e874
Topper Bowers <topper@toppingdesign.com>
2024-02-02 12:44:09 +0100

    fix speaking

--- sinky-explains-opensouls/soul/mentalProcesses/noticesTheTime.ts
+++ sinky-explains-opensouls/soul/mentalProcesses/noticesTheTime.ts
@@ -2,13 +2,17 @@
 import { ChatMessageRoleEnum, externalDialog } from "socialagi";
 import { MentalProcess, useActions, usePerceptions, useSoulMemory } from "soul-engine";
 
-const noticesTheTime: MentalProcess = async ({ step }) => {
-  const { scheduleEvent } = useActions()
+const noticesTheTime: MentalProcess = async ({ step: initialStep }) => {
+  const { scheduleEvent, log, speak } = useActions()
   const pendingScheduled = useSoulMemory("pendingScheduled", false)
   const { invokingPerception } = usePerceptions()
   if (!invokingPerception) {
     throw new Error("missing invoking perception, this should not happen")
   }
+
+  let step = initialStep
+
+  log("sinky notices the time")
 
   const time = new Date(invokingPerception._timestamp)
   // let's take a look at the last message
@@ -21,7 +25,9 @@ const noticesTheTime: MentalProcess = async ({ step }) => {
   }
 
   if (time.getTime() - timeOfLastUserMessage.getTime() > 30 * 1000) {
-    return step.next(externalDialog("Ask the user if you're boring them? Slinky says something interesting about themself."))
+    const { stream, nextStep } = await step.next(externalDialog("Ask the user if you're boring them? Slinky says something interesting about themself."), { stream: true })
+    speak(stream)
+    step = await nextStep
   }
 
   scheduleEvent({
