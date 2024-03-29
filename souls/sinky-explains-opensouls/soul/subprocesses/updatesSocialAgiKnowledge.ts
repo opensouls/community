501cb11293f45af1feacc833f9f0e7d69afa3227
thiago <371426+dooart@users.noreply.github.com>
2024-03-29 14:56:15 -0300

    sinky leftover

--- souls/sinky-explains-opensouls/soul/subprocesses/updatesSocialAgiKnowledge.ts
+++ souls/sinky-explains-opensouls/soul/subprocesses/updatesSocialAgiKnowledge.ts
@@ -1,12 +1,12 @@
 import { MentalProcess, useActions, useRag } from "@opensouls/engine";
 
-const updatesSocialAgiKnowledge: MentalProcess = async ({ step }) => {
+const updatesSocialAgiKnowledge: MentalProcess = async ({ workingMemory }) => {
   const { withRagContext } = useRag("example-sinky-says-awesome");
   const { log } = useActions();
 
   log("updating the working memory with knowledge from RAG");
 
-  return withRagContext(step);
+  return withRagContext(workingMemory);
 };
 
 export default updatesSocialAgiKnowledge;
