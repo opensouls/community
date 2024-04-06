011ddf5d41e916dba1ccb840d992a1e1c56242e4
Topper Bowers <topper@toppingdesign.com>
2024-04-06 22:50:22 +0200

    improve mentalquery

--- souls/example-twenty-questions/soul/lib/mentalQuery.ts
+++ souls/example-twenty-questions/soul/lib/mentalQuery.ts
@@ -11,8 +11,9 @@ const mentalQuery = createCognitiveStep((statement: string) => {
         role: ChatMessageRoleEnum.System,
         name: name,
         content: indentNicely`
-          Model the mind of ${name} and decide if ${name} would believe the following statement is true or false:
-
+          ${name} ponders the veracity of the following statement very carefully:
+          
+          ## Statement
           > ${statement}
 
           Please choose true if ${name} believes the statement is true, or false if ${name} believes the statement is false.
@@ -31,3 +32,34 @@ const mentalQuery = createCognitiveStep((statement: string) => {
 });
 
 export default mentalQuery
+// const mentalQuery = createCognitiveStep((statement: string) => {
+//   const params = z.object({
+//     isStatementTrue: z.boolean().describe(`Is the statement true or false?`),
+//   });
+
+//   return {
+//     command: ({ soulName: name }: WorkingMemory) => {
+//       return {
+//         role: ChatMessageRoleEnum.System,
+//         name: name,
+//         content: indentNicely`
+//           Model the mind of ${name} and decide if ${name} would believe the following statement is true or false:
+
+//           > ${statement}
+
+//           Please choose true if ${name} believes the statement is true, or false if ${name} believes the statement is false.
+//         `,
+//       };
+//     },
+//     schema: params,
+//     postProcess: async (memory: WorkingMemory, response: z.output<typeof params>) => {
+//       const newMemory = {
+//         role: ChatMessageRoleEnum.Assistant,
+//         content: `${memory.soulName} evaluated: \`${statement}\` and decided that the statement is ${response.isStatementTrue ? 'true' : 'false'}`
+//       };
+//       return [newMemory, response.isStatementTrue];
+//     }
+//   };
+// });
+
+// export default mentalQuery
