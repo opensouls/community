cc0ae74284afee4fb0d76b176ca2aace32503a44
thiago <371426+dooart@users.noreply.github.com>
2024-04-10 13:33:35 -0300

    fix favicon

--- demos/nextjs/reggie-is-regex/web/src/app/layout.tsx
+++ demos/nextjs/reggie-is-regex/web/src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import getAssetPath from "@/lib/assets";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Reggie The Regex",
   description: "A living regex existentially pondering the nature of regexes.",
+  icons: getAssetPath("/favicon.ico"),
 };
 
 export default function RootLayout({
